78f43c90488243ce3c4535161211246ce27195df
Saipavanganesh <pavanganeshms@gmail.com>
2023-07-09 12:00:42 +0530

    [Ganesh] add: Collections folder

---
+++ Collections Project/Collections Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArraylistDemo ald = new ArraylistDemo();
+            ald.alCollections();
+        }
+    }
+}
